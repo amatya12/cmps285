ecd1f99c7d61d4443e2560944780aa3513f7ac68
Aakash <aakash.amatya@selu.edu>
2018-09-15 21:44:45 -0500

    Initial commit

---
+++ EntityFrameWorkDatabaseSignup/Signup.cs
@@ -0,0 +1,27 @@
+namespace EntityFrameWorkDatabaseSignup
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Signup")]
+    public partial class Signup
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Email { get; set; }
+    }
+}
